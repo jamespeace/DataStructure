8a8dd892776cb940e02e4981296fad936769d2ef
jamespeace <james.happylove@gmail.com>
2019-11-05 00:50:40 +0800

    [Ch3] Implement Stack.

---
+++ chap3/cpp/Stack/Stack.h
@@ -0,0 +1,23 @@
+template <class T>
+class Stack {
+public:
+    Stack(int stackCapacity = 10);
+
+    bool IsEmpty() const;
+    void StackFull();
+
+    T& Top() const;
+    // Return the top element of stack
+
+    void Push(const T& item);
+    // Insert item into the top of the stack.
+
+    void Pop();
+    // Delete the top element of stack.
+
+private:
+    T *stack;
+    int top;
+    int capacity;
+};
+
