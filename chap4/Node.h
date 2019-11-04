64f2eaa5a2180cb5cc24be02426f464a57143866
jamespeace <james.happylove@gmail.com>
2019-11-05 00:51:50 +0800

    [Ch4] Implement Node.

---
+++ chap4/Node.h
@@ -0,0 +1,17 @@
+class NodeA {
+private:
+    int data1;
+    char data2;
+    float data3;
+    NodeA *linka;
+    NodeB *linkb;
+};
+
+class NodeB
+{
+private:
+    int data;
+    NodeB *link;
+};
+
+
