9dbb5132aaf291619dba92b6e977f226474eba30
jamespeace <james.happylove@gmail.com>
2019-11-14 01:19:31 +0800

    [Ch5] Add Dictionary.h

---
+++ chap5/Dictionary.h
@@ -0,0 +1,17 @@
+#ifndef _DICTIONARY_H_
+#define _DICTIONARY_H_
+
+template <class K, class E>
+class Dictionary {
+public:
+        // return true iff the dictionary is empty
+    virtual bool IsEmpty() const = 0;
+        // return pointer to the pair with specified key; return 0 if no such pair
+    virtual pair<K, E>* Get(const K&) const = 0;
+        // insert the given pair; if key is a duplicate update associated element
+    virtual Insert(const pair<K, E>&) = 0;
+        // delete pair with specified key
+    virtual void Delete(const K&) = 0;
+};
+
+#endif
