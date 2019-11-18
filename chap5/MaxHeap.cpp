5540be1617ca317a44c256cd9fcd468819486440
jamespeace <james.happylove@gmail.com>
2019-11-18 23:10:44 +0800

    [MaxHeap]Add Pop() and IsEmpty().

--- chap5/MaxHeap.cpp
+++ chap5/MaxHeap.cpp
@@ -37,3 +37,33 @@ void MaxHeap<T>::Push(const T& e)
     }
     heap[currentNode] = e;
 }
+
+template <class T>
+void MaxHeap<T>::Pop()
+{
+    // Delete max element.
+    if (IsEmpty()) throw "Heap is empty. Cannot delete.";
+    heap[1].~T()    // delete max element.
+
+    // remove last element from heap
+    T lastE = heap[heapSize] - 1;
+
+    // trickle down
+    int currentNode = 1;    // root
+    int child = 2;          // a child of currentNode
+    while (currentNode <= heapSize) {
+        // set child to larger child of currentNode
+        if (child <heapSize && heap[child] < heap[child+1])
+            child++;
+        
+        // can we put lastE in currentNode?
+        if (lastE >= heap[child])
+            break; // yes
+
+        // no
+        heap[currentNode] = heap[child];
+        currentNode = child;
+        child *= 2;
+    }
+    heap[currentNode] = lastE;
+}
--- chap5/MaxHeap.h
+++ chap5/MaxHeap.h
@@ -6,6 +6,8 @@ class MaxHeap {
 public:
     MaxHeap(int theCapacity = 10);
     void Push(const T& e);
+    void Pop();
+    bool IsEmpty() {return heapSize == 0;}
 private:
     T *heap;
     int heapSize;
