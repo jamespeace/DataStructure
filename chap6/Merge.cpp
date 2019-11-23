198ef60af04c4309d6e4675a43584e3276475e4c
jamespeace <james.happylove@gmail.com>
2019-11-23 21:57:56 +0800

    [Ch6]Add MergePass().

--- chap6/Merge.cpp
+++ chap6/Merge.cpp
@@ -18,3 +18,34 @@ void Merge(T *initList, T *mergedList, const int l, const int m, const int n)
     // copy remaining records, if any, of second list
     copy(initList + i2, initList + n + 1, mergedList + iResult)
 }
+
+template <class T>
+void MergePass(T *initList, T *resultList, const int n, const int s)
+{
+    // Adjcent pairs of sublists of size s are merged from
+    // initList to resultList. n is the number of recods in initList.
+    for (int i = 1; // i is first position in first of the sublists being merged
+         i <= n - 2*s + 1; // enough elements for two sublists of length s?
+         i += 2*s)
+        Merge(initList, resultList, i, i+s-1, i+2*s-1);
+
+    // merge remaining list of size < 2*s
+    if ((i + s - 1) < n)
+        Merge(initList, resultList, i, i + s - 1, n);
+    else
+        copy(initList + i, initList + n + 1, resultList + i);
+}
+
+template <class T>
+void MergeSort(T *a, const int n)
+{
+    // Sort a[1:n] into nondecreasing order.
+    T *tempList = new T[n+1];
+    // l is the length of the sublist currently being merged
+    for (int l = 1; l < n; l *= 2) {
+        MergePass(a, tempList, n, l);
+        l *= 2;
+        MergePass(tempList, a, n, l); // interchange role of a and tempList
+    }
+    delete []tempList;
+}
